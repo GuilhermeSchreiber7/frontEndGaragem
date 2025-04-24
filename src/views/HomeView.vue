8905e679c39b6d0aedce9d9f939cdd533d21acb8
GuilhermeSchreiber7 <guilhermeschreieber2007@gmail.com>
2025-04-24 13:27:56 -0300

    Feat: homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,14 +5,24 @@ import garagemHeader from '@/components/garagemHeader.vue';
 <template>
   <div class="app">
     <garagemHeader/>
-    <RouterView />
+    <div class="content">
+      Garagem
+    </div>
   </div>
 </template>
 <style scoped>
   .app{
     height: 100vh;
     width: 100vw;
-
     background-color: rgb(56, 56, 56)
+  }
+  .content {
+   width: 100%;
+    height: 90vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-size: 10rem;
   }
   </style>
